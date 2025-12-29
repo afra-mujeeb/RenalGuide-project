d7d219bcfb7d85a4ecfec26c0a17b2efb7a925b0
afra mujeeb <mujeebafra92@gmail.com>
2025-12-29 19:43:55 +0530

    29-12-2025

--- RenalGuideApp/models.py
+++ RenalGuideApp/models.py
@@ -120,7 +120,7 @@ class AppointmentTable(models.Model):
 
 
 class PrescriptionTable(models.Model):
-    prescription=models.CharField(max_length=30,null=True,blank=True)
+    prescription=models.TextField(null=True,blank=True)
     date=models.DateField(null=True,blank=True)
     APPOINTMENTID=models.ForeignKey(AppointmentTable,on_delete=models.CASCADE, null=True,blank=True)
 
